ef1b4f1d0e69e307c94abd57b7c6cdbe9bfd3958
valedict0 <jamesbrusewitz@gmail.com>
2024-10-20 14:02:44 -0700

    added menu scene (WIP)

---
+++ assets/menu/menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+# Menu should have multiple screens:
+#  - main title screen - we need a title
+#      - has buttons like play, quit, credits
+#  - pause menu screen (used during gameplay)
+#      - buttons like resume and quit to menu
+#  - game win screen
+#      - display score
+#  - game over screen
+#      - button return to menu
+#      - some display with score
+#  - credits w/ songs used, etc.
+# no need to program anything rn. use temporary art/blank white textures
+
+
