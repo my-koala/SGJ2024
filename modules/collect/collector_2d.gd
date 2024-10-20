3e66398ffbddf06fb1522074b7a775e5e43a909c
valedict0 <jamesbrusewitz@gmail.com>
2024-10-20 10:30:12 -0700

    Squashed commit of the following:
    commit 327438f262ce5de693c746a81081b292e3e750c8
    Author: PrahasP <122322605+PrahasP@users.noreply.github.com>
    Date:   Sat Oct 19 15:24:25 2024 -0700
    
        Moved Vacuum Code to collector_2D, and fixed some things
    
    commit 70a164884284fdaf166d600c7accd55a8b813a3c
    Author: PrahasP <122322605+PrahasP@users.noreply.github.com>
    Date:   Sat Oct 19 13:29:27 2024 -0700
    
        Made Vaccum Script
    
        Made the basic script for vaccuming in collectables

--- modules/collect/collector_2d.gd
+++ modules/collect/collector_2d.gd
@@ -1,20 +1,36 @@
 extends Area2D
 class_name Collector2D
 
+@export var vacuum_speed: float = 50.0 
+var candy_list: Array = []
+@export var collect_distance: float = 0.5
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 
-func _process(delta: float) -> void:
-	pass
-
 func _physics_process(delta: float) -> void:
-	pass
+	for collectable: Node2D in candy_list:
+		if is_instance_valid(collectable):
+			apply_vacuum(collectable)
 
 func _on_body_entered(body: Node2D) -> void:
-	pass
-	# if body is Collectable2D, then uhh delete it (for now) and increment a int counter
-	# we can do a ui to read this int later
+	if body is Collectable2D and is_instance_valid(body):
+		candy_list.append(body)
+		# print("Body entered: " + body.name)
 
 func _on_body_exited(body: Node2D) -> void:
-	pass
+	if body in candy_list:
+		candy_list.erase(body)
+		# print("Body exited: " + body.name)
+
+func apply_vacuum(collectable: RigidBody2D) -> void:
+	var direction: Vector2 = global_position - collectable.global_position 
+	var distance: float = direction.length()
+	
+	if distance < collect_distance: # IDK how far that actually is lol
+		print("add the candy to the player in here")
+
+	if distance > 1.0: # From what I've seen, this part makes it look nicer
+		direction = direction.normalized()
+		collectable.apply_force(direction * vacuum_speed, Vector2.ZERO) 
